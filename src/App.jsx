5e8d5da593562ec08c5e7ecc1d955d949773708f
Daniel <70490862+catoverhat@users.noreply.github.com>
2023-05-09 18:33:29 -0400

    Refactor the display and add new features

--- src/App.jsx
+++ src/App.jsx
@@ -5,54 +5,145 @@ import Buttons from "./components/Buttons";
 import "./App.css";
 
 function App() {
-  const [displayValue, setDisplayValue] = useState([]);
+  const [smallDisplayValue, setSmallDisplayValue] = useState([]);
   const [isResult, setIsResult] = useState(false);
   const [calculations, setCalculations] = useState({
-    value1: [],
+    value1: "0",
     operand: "",
-    value2: [],
+    value2: "",
+    result: "",
   });
 
+  const clear = () => {
+    setIsResult(false);
+    setCalculations({
+      value1: "0",
+      operand: "",
+      value2: "",
+      result: "",
+    });
+  };
+
+  const equal = () => {
+
+    if (calculations.operand === "+") {
+      setCalculations((prevState) => ({
+        ...prevState,
+        result: calculations.value1 + calculations.value2,
+      }));
+    }
+    if (calculations.operand === "-") {
+      setCalculations((prevState) => ({
+        ...prevState,
+        result: calculations.value1 - calculations.value2,
+      }));
+    }
+    if (calculations.operand === "x") {
+      setCalculations((prevState) => ({
+        ...prevState,
+        result: calculations.value1 * calculations.value2,
+      }));
+    }
+    if (calculations.operand === "÷") {
+      setCalculations((prevState) => ({
+        ...prevState,
+        result: calculations.value1 / calculations.value2,
+      }));
+    }
+  };
+
   const handleButtonClicked = ({ target: { textContent } }) => {
-    if (textContent === "+" || textContent === "-") {
+    if (
+      calculations.value1 &&
+      calculations.operand &&
+      calculations.value2 &&
+      textContent === "="
+    ) {
+      setIsResult(true);
+      equal();
+      setCalculations((prevState) => ({
+        ...prevState,
+        operand: "",
+      }));
+    } else if (calculations.result && !isNaN(parseInt(textContent))) {
+      clear();
+      setCalculations((prevState) => ({
+        ...prevState,
+        value1: parseInt(prevState.value1 + textContent),
+      }));
+    } else if (
+      calculations.result &&
+      (textContent === "+" ||
+        textContent === "-" ||
+        textContent === "x" ||
+        textContent === "÷")
+    ) {
+      setIsResult(false);
+      setCalculations((prevState) => ({
+        ...prevState,
+        value1: prevState.result,
+        operand: textContent,
+        value2: "",
+        result: "",
+      }));
+    } else if (
+      calculations.value1 &&
+      calculations.operand &&
+      calculations.value2 &&
+      (textContent === "+" ||
+        textContent === "-" ||
+        textContent === "x" ||
+        textContent === "÷")
+    ) {
+      setIsResult(false);
+      equal()
+      // console.log('calculations')
+      setCalculations((prevState) => ({
+        ...prevState,
+        value1: prevState.result,
+        operand: textContent,
+        value2: "",
+        result: "",
+      }));
+    } else if (
+      calculations.value1 &&
+      (textContent === "+" ||
+        textContent === "-" ||
+        textContent === "x" ||
+        textContent === "÷")
+    ) {
       setCalculations((prevState) => ({
         ...prevState,
         operand: textContent,
       }));
-    } else if (!calculations.operand && calculations.operand === "") {
+    } else if (calculations.operand && !isNaN(parseInt(textContent))) {
       setCalculations((prevState) => ({
         ...prevState,
-        value1: [...prevState.value1, textContent],
+        value2: parseInt(prevState.value2 + textContent),
       }));
-    } else if (calculations.operand) {
+    } else if (!isNaN(parseInt(textContent))) {
       setCalculations((prevState) => ({
         ...prevState,
-        value2: [...prevState.value2, textContent],
+        value1: parseInt(prevState.value1 + textContent),
       }));
     }
-    if (textContent === "=") {
-      if(calculations.operand === '+'){
-        console.log(parseInt(calculations.value1.join('')) + parseInt(calculations.value2))
-      }
-    }
-
     if (textContent === "clear") {
-      setDisplayValue([]);
-      setCalculations({
-        value1: [],
-        operand: "",
-        value2: [],
-      });
-      setIsResult(false);
-    } else {
-      setDisplayValue((prevState) => [...prevState, textContent]);
+      clear();
     }
     console.log(calculations);
   };
 
   return (
     <Body>
-      <Screen displayButtons={displayValue} />
+      <Screen
+        displayButtons={
+          isResult
+            ? calculations.result
+            : calculations.value2
+            ? calculations.value2
+            : calculations.value1
+        }
+      />
       <Buttons buttonClicked={handleButtonClicked} />
     </Body>
   );
